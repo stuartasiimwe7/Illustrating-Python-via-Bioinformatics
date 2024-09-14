cce76b92b5eb8c69f7b9cd8dbc68249ea55d4a27
Stuart Asiimwe <stuartgabriel@ymail.com>
2024-09-15 06:32:44 +0900

    percolatory py

--- percolator/percolator.py
+++ percolator/percolator.py
@@ -1,8 +1,8 @@
-# %% [markdown]
-# ### Preprocessing File# %%
+import subprocess
+
+#Preprocessing File#
 file_name = "Insert_your_file_here.pin"
 
-# %%
 def preprocess(file_name):
 
     file = open(file_name, "r")
@@ -15,28 +15,18 @@ def preprocess(file_name):
     file_write.close()
     file.close()
 
-
-# %%
 preprocess(file_name) #calling the method
 
-# %% [markdown]
-# ### Calling the Percolator
 
-# %% [markdown]
-# ##### There comes a time when you need to inclide the percolator into the python code instead of running in the CMD for pre-pre-processing
+#Calling the Percolator
+#There comes a time when you need to inclide the percolator into the python code instead 
+#of running in the CMD for pre-pre-processing
 
-# %%
-import subprocess
-
-# %%
 #path to the input file
 input_file = 'new_input.pin'
-
-# %%
 #command to call Percolator
 call = ['percolator', input_file]
 
-# %%
 #executing the command with try & exemptions check
 try:
     output = subprocess.check_output(call, universal_newlines=True)
